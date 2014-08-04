5f2d14920c308a077bdef66b6a7001470b9fe572
Ben Drucker <bvdrucker@gmail.com>
2014-08-03 20:52:12 -0400

    Port to gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp       = require('gulp');
+var plugins    = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var source     = require('vinyl-source-stream');
+var buffer     = require('vinyl-buffer');
+var fs         = require('fs');
+
+gulp.task('bundle', function () {
+  return browserify({
+    standalone: 'mockfirebase'
+  })
+  .add('./src/MockFirebase.js')
+  .bundle()
+  .pipe(source('MockFirebase.js'))
+  .pipe(buffer())
+  .pipe(plugins.header(fs.readFileSync('./helpers/header.txt', 'utf8'), {
+    pkg: require('./package.json')
+  }))
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('cover', function () {
+  return gulp.src('./src/**/*.js')
+    .pipe(plugins.istanbul());
+});
+
+gulp.task('test', ['cover'], function () {
+  return gulp.src('test/**/*.js')
+    .pipe(plugins.mocha())
+    .pipe(plugins.istanbul.writeReports());
+});
---
+++ helpers/globals.js
@@ -0,0 +1,6 @@
+'use strict';
+
+if (typeof window === 'object') {
+  window.MockFirebase = window.mockfirebase.MockFirebase;
+  window.MockFirebaseSimpleLogin = window.mockfirebase.MockFirebaseSimpleLogin;
+}
