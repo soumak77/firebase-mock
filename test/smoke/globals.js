a350b2d3d1d2b189187fd44ced1756eac0bd84b0
Ben Drucker <bvdrucker@gmail.com>
2014-10-28 07:26:08 -0400

    Whitelist globals vars in window global smoke test

--- test/smoke/globals.js
+++ test/smoke/globals.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/* jshint browser:true */
+/* globals expect:false, mockfirebase:false */
+
 describe('Custom UMD Build', function () {
 
   it('exposes the full module as "mockfirebase"', function () {
