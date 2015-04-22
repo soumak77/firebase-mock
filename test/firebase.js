b9269f387288a8ade0911e072b5721c9fe2b74ad
Ben Drucker <bvdrucker@gmail.com>
2015-04-22 08:53:27 -0400

    Wrap Firebase and URL tests in a test block for organization

--- test/firebase.js
+++ test/firebase.js
@@ -4,11 +4,14 @@ import test from 'tape'
 import startsWith from 'core-js/fn/string/starts-with'
 import Firebase from '../'
 
-test('Constructor', (t) => {
-  let ref = new Firebase()
-  t.test('url', (t) => {
-    t.ok(startsWith(ref.url, 'mock://'), 'defaults to mock protocol')
-    t.notEqual(ref.url, new Firebase().url, 'defaults to random host')
+test('Firebase', (t) => {
+  t.test('Constructor', (t) => {
+    let ref = new Firebase()
+    t.test('url', (t) => {
+      t.ok(startsWith(ref.url, 'mock://'), 'defaults to mock protocol')
+      t.notEqual(ref.url, new Firebase().url, 'defaults to random host')
+      t.end()
+    })
     t.end()
   })
   t.end()
--- test/url.js
+++ test/url.js
@@ -3,11 +3,14 @@
 import test from 'tape'
 import * as url from '../src/url'
 
-test('parse', (t) => {
-  const parsed = url.parse('mock://host/path')
-  t.deepEqual(parsed, {
-    endpoint: 'mock://host',
-    path: '/path'
-  }, 'parses to endpoint and path')
+test('url', (t) => {
+  t.test('parse', (t) => {
+    const parsed = url.parse('mock://host/path')
+    t.deepEqual(parsed, {
+      endpoint: 'mock://host',
+      path: '/path'
+    }, 'parses to endpoint and path')
+    t.end()
+  })
   t.end()
 })
