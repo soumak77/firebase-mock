dea0f09dfb4324436555f998b8bc802c53b249e2
Ben Drucker <bvdrucker@gmail.com>
2015-04-28 14:31:18 -0400

    Remove control over individual events and make the flush queue a simple Set

--- src/queue.js
+++ src/queue.js
@@ -1,47 +1,20 @@
 'use strict'
 
-import {EventEmitter} from 'events'
-
-class FlushQueue {
+export default class FlushQueue extends Set {
   constructor () {
-    this.events = new Set()
+    super()
     this.flushing = false
-  }
-  add (event) {
-    let {fn, context, sourceData} = event
-    event = new FlushEvent(fn, context, sourceData)
-      .once('done', (event) => {
-        this.events.delete(event)
-      })
-    this.events.add(event)
-    return this
   }
   flush () {
     if (this.flushing) return
-    if (!this.events.size) {
+    if (!this.size) {
       throw new Error('No deferred tasks to be flushed')
     }
     this.flushing = true
-    for (let event of this.events) event.run()
+    for (let fn of this) {
+      fn()
+    }
+    this.clear()
     this.flushing = false
   }
-  getEvents () {
-    return [...this.events]
-  }
 }
-
-class FlushEvent extends EventEmitter {
-  constructor (fn, context, sourceData) {
-    super()
-    Object.assign(this, {fn, context, sourceData})
-  }
-  run () {
-    this.fn.call(this.context)
-    this.emit('done', this)
-  }
-  cancel () {
-    this.emit('done', this)
-  }
-}
-
-export {FlushQueue as Queue, FlushEvent as Event}
--- test/queue.js
+++ test/queue.js
@@ -2,62 +2,28 @@
 
 import test from 'tape'
 import {spy} from 'sinon'
-import {Queue, Event} from '../src/queue'
+import Queue from '../src/queue'
 
 test('FlushQueue', (t) => {
   const queue = new Queue()
-  t.equal(queue.events.size, 0, 'initially empty')
-  t.equal(queue.add({}).events.size, 1, 'can add events')
   t.test('flush', (t) => {
-    queue.events.clear()
     t.throws(queue.flush.bind(queue), 'No deferred', 'throws when empty')
     const fn = spy()
-    queue.add({fn})
+    queue.add(fn)
     queue.flush()
     t.ok(fn.called, 'fires events synchronously')
-    t.equal(queue.events.size, 0, 'clears itself when run')
+    t.equal(queue.size, 0, 'clears after run')
     fn.reset()
-    queue.add({fn: () => {
-      queue.add({fn: fn}) // can't be destructured (output becomes circular)
-    }})
+    queue.add(() => {
+      queue.add(fn)
+    })
     queue.flush()
     t.ok(fn.called, 'fires events added during processing')
-    queue.add({fn: () => {
+    queue.add(() => {
       queue.flush()
-    }})
+    })
     queue.flush()
     t.pass('prevents recursive flush')
-    t.end()
-  })
-  t.test('getEvents', (t) => {
-    queue.events.clear()
-    const fn = spy()
-    queue.add({fn})
-    const events = queue.getEvents()
-    t.equal(events[0].fn, fn, 'gets events')
-    t.end()
-  })
-  t.end()
-})
-
-test('FlushEvent', (t) => {
-  const fn = spy()
-  const context = {}
-  const event = new Event(fn, context)
-  t.test('run', (t) => {
-    event.run()
-    t.equal(fn.firstCall.thisValue, context, 'calls on context')
-    const listener = spy()
-    event.on('done', listener)
-    event.run()
-    t.ok(listener.called, 'emits "done" event')
-    t.end()
-  })
-  t.test('cancel', (t) => {
-    const listener = spy()
-    event.on('done', listener)
-    event.cancel()
-    t.ok(listener.called, 'emits "done" event')
     t.end()
   })
   t.end()
