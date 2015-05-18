07b8eee3afc7a6df32657e49de5061eea26efecd
Ben Drucker <bvdrucker@gmail.com>
2015-05-18 17:34:55 +0200

    Don't use ES7 bind syntax (not yet linted)

--- src/clock.js
+++ src/clock.js
@@ -1,13 +1,13 @@
 'use strict'
 
 import Clock from 'clockwise'
-import {sv1} from 'value-to-firebase'
-import {sv2} from 'object-to-firebase'
+import {sv as sv1} from 'value-to-firebase'
+import {sv as sv2} from 'object-to-firebase'
 
 const clock = new Clock()
 
 ;[sv1, sv2]
   .map(sv => sv.values)
-  .forEach(values => values.timestamp = ::clock.time)
+  .forEach(values => values.timestamp = clock.time.bind(clock))
 
 export default clock
