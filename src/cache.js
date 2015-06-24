935e78d77578a4e5cfc1b57baf3805594c8824fe
Ben Drucker <bvdrucker@gmail.com>
2015-06-23 22:02:13 -0700

    es5ify cache

--- src/cache.js
+++ src/cache.js
@@ -1,30 +1,38 @@
 'use strict'
 
-export default class RefCache {
-  constructor () {
-    this.store = new Map()
-    this.disable()
-  }
-  enable () {
-    this.enabled = true
-  }
-  disable () {
-    this.enabled = false
-  }
-  clear () {
-    for (let key of this.store.keys()) {
-      this.store.delete(key)
-    }
-  }
-  get () {
-    if (!this.enabled) return
-    return this.store.get(...arguments)
-  }
-  set () {
-    if (!this.enabled) return this
-    return this.store.set(...arguments)
-  }
-  get size () {
+module.exports = RefCache
+
+function RefCache () {
+  this.store = new Map()
+  this.disable()
+}
+
+RefCache.prototype.enable = function () {
+  this.enabled = true
+}
+
+RefCache.prototype.disable = function () {
+  this.enabled = false
+}
+
+RefCache.prototype.clear = function () {
+  this.store.forEach(function (value, key) {
+    this.store.delete(key)
+  }, this)
+}
+
+RefCache.prototype.get = function () {
+  if (!this.enabled) return
+  return this.store.get.apply(this.store, arguments)
+}
+
+RefCache.prototype.set = function () {
+  if (!this.enabled) return this
+  return this.store.set.apply(this.store, arguments)
+}
+
+Object.defineProperty(RefCache.prototype, 'size', {
+  get: function () {
     return this.store.size
   }
-}
+})
