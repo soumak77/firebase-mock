31253d8604e5bf213dfce89136323fe11ad0502e
Tyler Jones <tylerjones64@gmail.com>
2018-07-02 17:09:12 -0700

    implement firestore.getAll mock

--- test/unit/firestore.js
+++ test/unit/firestore.js
@@ -186,4 +186,25 @@ describe('MockFirestore', function () {
       db.flush();
     });
   });
+
+  describe('getAll', function() {
+    it('', function() {
+      var doc1 = db.doc('doc1');
+      var doc2 = db.doc('doc2');
+      var doc3 = db.doc('doc3');
+
+      db.autoFlush();
+
+      doc1.set({value: 1});
+      doc2.set({value: 2});
+
+      return db
+        .getAll(doc1, doc2, doc3)
+        .then(function(snaps) {
+          expect(snaps[0].data()).to.deep.equal({value: 1});
+          expect(snaps[1].data()).to.deep.equal({value: 2});
+          expect(snaps[2].exists).to.equal(false);
+        });
+    });
+  });
 });
