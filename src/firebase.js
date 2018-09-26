f09bf69de9b62bcf958955f1592e6a976b4bd540
Rich Hodgkins <rhodgkins@gmail.com>
2018-09-26 20:32:29 +0100

    Fixed #110 - Remove leading slashes in updates before using key

--- src/firebase.js
+++ src/firebase.js
@@ -194,7 +194,7 @@ MockFirebase.prototype.update = function (changes, callback) {
         // operate as a multi-set
         _.keys(changes).forEach(function (key) {
           var val = changes[key];
-          _.set(data, key.replace(/\//g, '.'), _.isObject(val) ? utils.updateToRtdbObject(val) : val);
+          _.set(data, key.replace(/^\//, '').replace(/\//g, '.'), _.isObject(val) ? utils.updateToRtdbObject(val) : val);
         });
         data = utils.removeEmptyRtdbProperties(data);
         self._dataChanged(data);
